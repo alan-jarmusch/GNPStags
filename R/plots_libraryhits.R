41a7c2eb18b61257a464335fdc402f87b4603b20
alan-jarmusch <41308046+alan-jarmusch@users.noreply.github.com>
2020-06-23 00:24:22 -0700

    Add files via upload
    update issues in separate for template plots

---
+++ R/plots_libraryhits.R
@@ -0,0 +1,16 @@
+#' Produce Plots from Library Hits
+#'
+#' This function works to generate plots which summarize the 4 different types of tags, including the GNPS Tags, NCBI taxonomy tags, UBERON tags, and lifestyle tags contribtued by users.
+#' @param path Path to the downloaded table of "View all Library Hits" from molecular networking (classic), library search workflows, or feature-based molecular networking in GNPS.
+#' @keywords
+#' @export
+#' @examples plots_libraryhit(path = "METABOLOMICS-SNETS-V2-f1a1f3a6-view_all_annotations_DB-main.tsv")
+#' @author Alan K. Jarmusch 2020
+#' @export
+#'
+plots_libraryhit <- function(path) {
+  summary_TAGS_libraryhits()
+  summary_UBERON_libraryhits()
+  summary_NCBI_libraryhits()
+  summary_Lifestyle_libraryhits()
+}
